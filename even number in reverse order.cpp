c008f714cb27535e53708a47861e158ecce02d51
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-05-09 12:41:53 +0530

    To find the even number in reverse order .

---
+++ even number in reverse order.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+    int i;   
+    for(i = 20; i >= 1; i--)
+	{    
+      if(i % 2 == 0) 
+	  {
+         cout << i <<" "; 
+      }
+   }
+    return 0;
+}
